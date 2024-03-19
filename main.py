90152f85b5b8c41dda936b145b007111e0e50007
sean15535 <sean15535@gmail.com>
2024-03-19 13:55:17 +0100

    code for the game

---
+++ main.py
@@ -0,0 +1,13 @@
+
+print("Welocme to Guess the Number Game")
+while True:
+    print("1. Play Game \n2. About Game \n3. Exit")
+    response = int(input(">>> "))
+    if response == 1:
+        print()
+    elif response == 2:
+        print()
+    elif response == 3:
+        print()
+    else:
+        print("invalid input")
