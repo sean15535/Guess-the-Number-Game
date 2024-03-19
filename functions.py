7b272c73ba268f876ec7d441bb3a8870f9aa88f6
sean15535 <sean15535@gmail.com>
2024-03-19 14:41:21 +0100

    modified the functions

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ def play():
     guess_limit=3
     while guess_limit > 0:
         user= int(input("Enter your guess Number: "))
-        computer = random.choice([range(10)])
+        computer = random.choice(range(10))
         if user == computer:
             print(f"Guess Number is {computer}, You won!")
             return
@@ -13,5 +13,9 @@ def play():
             print(f"Guess Number is {computer}, You Lose! \nAttempts remains {guess_limit}")
     print("Attempt exceeded. Game over ")
 
-    
+def about():
+    print("This is a simple guessing game where you have to guess a number between 0 and 9. \nYou have 3 attempts to guess the correct number.")
+
+def exit():
+    print("Exiting App")    
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-
+from functions import play, about, exit
 print("Welocme to Guess the Number Game")
 while True:
     print("1. Play Game \n2. About Game \n3. Exit")
     response = int(input(">>> "))
     if response == 1:
-        print()
+        play()
     elif response == 2:
-        print()
+        about()
     elif response == 3:
-        print()
+        exit()
+        break
     else:
         print("invalid input")
